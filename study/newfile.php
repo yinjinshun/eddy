b764908841b608a6e68a1bbb4fcdd5e505d549cb
yinjinshun <yinjinshun@etcp.cn>
2017-12-28 17:05:29 +0800

    添加初始文件

---
+++ study/newfile.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @文件名称: study.php
+ * @author: Administrator
+ * @Date: 2017年12月28日
+ * @Copyright: 2017 悦畅科技有限公司. All rights reserved.
+ * 注意：本内容仅限于悦畅科技有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+class study{
+    public function __construct(){
+        
+    }
+    
+    public function run(){
+        echo 'this is app';
+    }
+}
